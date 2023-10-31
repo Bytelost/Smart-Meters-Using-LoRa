74ded13cd2482e497bfb530cc65ddd090de9e457
Vinicius Muller de Freitas <viniciusmullerdefreitas309@gmail.com>
2023-10-31 14:38:21 -0300

    change project structure

--- PicoMQTT/test2.py
+++
@@ -1,7 +0,0 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client(client_id="02", clean_session=True, userdata=None, transport="tcp")
-client.connect("192.168.117.247", 1883, 10)
-
-client.subscribe("test/topic")
-client.publish("test/topic", "Hello World!")
