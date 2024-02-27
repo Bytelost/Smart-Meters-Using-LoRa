ba6ebf50c426cdcbd165267e38c3c1682104c1a6
Vinicius Muller de Freitas <viniciusmullerdefreitas309@gmail.com>
2024-02-27 18:25:32 -0300

    fix mqtt not working

--- PicoMQTT Standalone/src/main.cpp
+++ PicoMQTT Standalone/src/main.cpp
@@ -10,10 +10,12 @@
 
 #define  LMIC_DEBUG_LEVEL = 1 
 #define CFG_au915
+#define LORA_GAIN 14 
 
 void do_send(osjob_t* j);
 void buildPacket(uint8_t *txBuffer);
 void ShowMsg();
+void setupLoRaWAN();
 
 // Compile Regression Settings
 #ifdef COMPILE_REGRESSION_TEST
@@ -23,22 +25,26 @@ void ShowMsg();
 #define FILLMEIN (#dont edit this, edit the lines that use FILLMEIN)
 #endif
 
-// APPEUI in LSB
-static const u1_t PROGMEM APPEUI[8] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-void os_getArtEui(u1_t *buf) { memcpy_P(buf, APPEUI, 8); }
+// LoRaWAN NwkSKey, network session key
+static const PROGMEM u1_t NWKSKEY[16] = {0xED, 0x4D, 0xD7, 0x9F, 0x67, 0x2A, 0x23, 0xCF, 0x99, 0xD6, 0xC5, 0x89, 0x3E, 0x46, 0xE5, 0x19};
 
-// DEVEUI in LSB
-static const u1_t PROGMEM DEVEUI[8] = {0x8F, 0x55, 0x06, 0xD0, 0x7E, 0xD5, 0xB3, 0x70};
-void os_getDevEui(u1_t *buf) { memcpy_P(buf, DEVEUI, 8); }
+// LoRaWAN AppSKey, application session key
+static const u1_t PROGMEM APPSKEY[16] = {0x56, 0x71, 0xB9, 0x07, 0x15, 0xD0, 0xA7, 0xA3, 0x5C, 0xEF, 0x05, 0x51, 0x03, 0xE9, 0xE1, 0x9D};
 
-// APPKEY in MSB
-static const u1_t PROGMEM APPKEY[16] = {0xE4, 0xE8, 0xFB, 0x67, 0x8E, 0x36, 0x2D, 0x40, 0x82, 0xAE, 0x67, 0x63, 0x27, 0x26, 0xD3, 0x3D};
-void os_getDevKey(u1_t *buf) { memcpy_P(buf, APPKEY, 16); }
+// LoRaWAN end-device address (DevAddr)
+static const u4_t DEVADDR = 0x260DDE71; 
 
+// Only used for OTTA activation
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+// Message to send
 char tempString[16];
-static osjob_t sendjob;
 static uint8_t txBuffer[9];
-bool flag = false; // Flag para debounce
+
+
+static osjob_t sendjob;
 
 // LoRa transmission time
 const unsigned TX_INTERVAL = 30;
@@ -50,6 +56,110 @@ const lmic_pinmap lmic_pins = {
     .rst = 14, 
     .dio = {26, 35, 34},
 };
+
+//OLED Settings
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+// WiFi Settings
+const char* WIFI_SSID = "Standalone-Broker";
+const char* WIFI_PASSWORD = "123456789ac";
+
+// Loop variables
+unsigned int update_time = 0;
+uint8_t counter = 0;
+
+// MQTT Settings
+PicoMQTT :: Server mqtt(1818);
+
+void setup() {
+  // Setup serial
+  Serial.begin(115200);
+  Serial.println("Starting...");
+  
+  // Setup WiFi
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(WIFI_SSID, NULL);
+
+  // Setup LoRa
+  setupLoRaWAN();
+
+  // Setup OLED
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  // Initialize the OLED display using Wire library
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;); // Don't proceed, loop forever
+  }
+
+  //Display text
+  ShowMsg();  
+
+  // Subscribe to MQTT topics
+  mqtt.subscribe("topic/message", [](const char * topic, const char * payload) {
+      Serial.printf("Received message in topic '%s': %s\n", topic, payload);
+      
+  });
+
+  // Start mqtt broker
+  mqtt.begin();
+
+}
+
+void loop() {
+  mqtt.loop();
+
+  // Update OLED every 5 seconds
+  if (millis() - update_time > 5000) {
+    update_time = millis();
+    ShowMsg();
+  }
+
+}
+
+void OledClear() {
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+}
+
+void buildPacket(uint8_t txBuffer[9])
+{
+  
+  txBuffer[0] = tempString[0]; 
+  txBuffer[1] = tempString[1];
+  txBuffer[2] = tempString[2];
+  txBuffer[3] = tempString[3];
+  txBuffer[4] = tempString[4];
+
+}
+
+void ShowMsg() {
+  OledClear();
+  int num = WiFi.softAPgetStationNum();
+  display.print("Standalone-Broker\n");
+  display.printf("IP: %s\n", WiFi.softAPIP().toString().c_str());
+  display.printf("PORT: 1818\n");
+  display.printf("Clients: %d\n", num);
+  display.display();
+}
+
+void printHex2(unsigned v) {
+  v &= 0xff;
+  if (v < 16)
+    Serial.print('0');
+  Serial.print(v, HEX);
+}
 
 // LoRaWAN event handler
 void onEvent (ev_t ev) {
@@ -135,138 +245,117 @@ void onEvent (ev_t ev) {
   }
 }
 
-// LoRa Send Data
-void do_send(osjob_t* j) { 
-
-  // Check if there is not a current TX/RX job running 
+void do_send(osjob_t* j) {  
+  // Check if there is not a current TX/RX job running
   if (LMIC.opmode & OP_TXRXPEND)
   {
     Serial.println(F("OP_TXRXPEND, not sending"));
+    //LoraStatus = "OP_TXRXPEND, not sending";
   }
   else
-  {
+  {       
     buildPacket(txBuffer);
+    
     LMIC_setTxData2(1, txBuffer, sizeof(txBuffer), 0);
     Serial.println(F("Packet queued"));
+    //LoraStatus = "Packet queued";
   }
   // Next TX is scheduled after TX_COMPLETE event.
 }
 
-//OLED Settings
-#define OLED_SDA 4
-#define OLED_SCL 15 
-#define OLED_RST 16
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
-
-// WiFi Settings
-const char* WIFI_SSID = "Standalone-Broker";
-const char* WIFI_PASSWORD = "123456789";
-
-// Loop variables
-unsigned int update_time = 0;
-
-// MQTT Settings
-PicoMQTT :: Server mqtt(1818);
-
-void setup() {
-  // Setup serial
-  Serial.begin(9600);
-  Serial.println("Starting...");
-  
-  // Setup WiFi
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(WIFI_SSID, NULL);
-
-  // Setup OLED
-  pinMode(OLED_RST, OUTPUT);
-  digitalWrite(OLED_RST, LOW);
-  delay(20);
-  digitalWrite(OLED_RST, HIGH);
-
-  // Initialize the OLED display using Wire library
-  Wire.begin(OLED_SDA, OLED_SCL);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
-      Serial.println(F("SSD1306 allocation failed"));
-      for(;;); // Don't proceed, loop forever
-  }
-
-  //Display text
-  ShowMsg();  
-
-  // Subscribe to MQTT topics
-  mqtt.subscribe("topic/message", [](const char * topic, const char * payload) {
-      Serial.printf("Received message in topic '%s': %s\n", topic, payload);
-      
-      // Send message to LoRa
-      strcpy(tempString, payload);
-      do_send(&sendjob);
-      
-  });
-
-  // Setup LoRa
-  pinMode(0, INPUT_PULLUP);
-
-  #ifdef VCC_ENABLE
-    // For Pinoccio Scout boards
-    pinMode(VCC_ENABLE, OUTPUT);
-    digitalWrite(VCC_ENABLE, HIGH);
-    delay(1000);
-  #endif
-
+// LoRaWAN Setup
+void setupLoRaWAN(){
   // LMIC init
   os_init();
   // Reset the MAC state. Session and pending data transfers will be discarded.
   LMIC_reset();
 
-}
+  // Set static session parameters. Instead of dynamically establishing a session
+  // by joining the network, precomputed session parameters are be provided.
+#ifdef PROGMEM
+  // On AVR, these values are stored in flash and only copied to RAM
+  // once. Copy them to a temporary buffer here, LMIC_setSession will
+  // copy them into a buffer of its own again.
+  uint8_t appskey[sizeof(APPSKEY)];
+  uint8_t nwkskey[sizeof(NWKSKEY)];
+  memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+  memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+  LMIC_setSession (0x13, DEVADDR, nwkskey, appskey);
+#else
+  // If not running an AVR with PROGMEM, just use the arrays directly
+  LMIC_setSession (0x13, DEVADDR, NWKSKEY, APPSKEY);
+#endif
 
-void loop() {
-  mqtt.loop();
+#if defined(CFG_eu868)
+  // Set up the channels used by the Things Network, which corresponds
+  // to the defaults of most gateways. Without this, only three base
+  // channels from the LoRaWAN specification are used, which certainly
+  // works, so it is good for debugging, but can overload those
+  // frequencies, so be sure to configure the full frequency range of
+  // your network here (unless your network autoconfigures them).
+  // Setting up channels should happen after LMIC_setSession, as that
+  // configures the minimal channel set. The LMIC doesn't let you change
+  // the three basic settings, but we show them here.
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band  
+ // LMIC_setupChannel(8, 994000000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  2);      // g2-band
+  // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+  // devices' ping slots. LMIC does not have an easy way to define set this
+  // frequency and support for class B is spotty and untested, so this
+  // frequency is not configured here.
+#elif defined(CFG_us915) || defined(CFG_au915)
+  // NA-US and AU channels 0-71 are configured automatically
+  // but only one group of 8 should (a subband) should be active
+  // TTN recommends the second sub band, 1 in a zero based count.
+  // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+  LMIC_selectSubBand(1);
 
-  // Update OLED every 5 seconds
-  if (millis() - update_time > 5000) {
-    update_time = millis();
-    ShowMsg();
-  }
+#elif defined(CFG_as923)
+  // Set up the channels used in your country. Only two are defined by default,
+  // and they cannot be changed.  Use BAND_CENTI to indicate 1% duty cycle.
+  // LMIC_setupChannel(0, 923200000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
+  // LMIC_setupChannel(1, 923400000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
 
-  std::string tempString = "123";
+  // ... extra definitions for channels 2..n here
+#elif defined(CFG_kr920)
+  // Set up the channels used in your country. Three are defined by default,
+  // and they cannot be changed. Duty cycle doesn't matter, but is conventionally
+  // BAND_MILLI.
+  // LMIC_setupChannel(0, 922100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+  // LMIC_setupChannel(1, 922300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+  // LMIC_setupChannel(2, 922500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+
+  // ... extra definitions for channels 3..n here.
+#elif defined(CFG_in866)
+  // Set up the channels used in your country. Three are defined by default,
+  // and they cannot be changed. Duty cycle doesn't matter, but is conventionally
+  // BAND_MILLI.
+  // LMIC_setupChannel(0, 865062500, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+  // LMIC_setupChannel(1, 865402500, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+  // LMIC_setupChannel(2, 865985000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+
+  // ... extra definitions for channels 3..n here.
+#else
+# error Region not supported
+#endif
+
+  // Disable link check validation
+  LMIC_setLinkCheckMode(0);
+
+  // TTN uses SF9 for its RX2 window.
+  LMIC.dn2Dr = DR_SF9;
+
+  // Set data rate and transmit power for uplink
+  LMIC_setDrTxpow(DR_SF10, LORA_GAIN);
+
+  // Start job
   do_send(&sendjob);
-
-}
-
-void OledClear() {
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0,0);
-}
-
-void ShowMsg() {
-  OledClear();
-  int num = WiFi.softAPgetStationNum();
-  display.print("Standalone-Broker\n");
-  display.printf("IP: %s\n", WiFi.softAPIP().toString().c_str());
-  display.printf("PORT: 1818\n");
-  display.printf("Clients: %d\n", num);
-  display.display();
-}
-
-void buildPacket(uint8_t txBuffer[9])
-{
-  Serial.println(F("Building packet"));
-  if(sizeof(tempString) > (sizeof(txBuffer)-1))
-  {
-    Serial.println(F("Payload too long"));
-    return;
-  }
-  else
-  {
-    txBuffer[0] = 0x01;
-    for(int i = 0; i < sizeof(tempString); i++)
-    {
-      txBuffer[i+1] = tempString[i];
-    }
-  }
+ 
 }
