5e74e067f63cb0f4cec1c4a99d6f73df35ffb3ed
Vinicius Muller de Freitas <viniciusmullerdefreitas309@gmail.com>
2024-07-01 15:33:35 -0300

    save the messages and fet the time

--- get-data.py
+++ get-data.py
@@ -1,21 +1,43 @@
 import paho.mqtt.client as mqtt
 import json
+from datetime import datetime
 
+file_path = 'data.json'
+
+# Conection callback
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     client.subscribe("v3/netzer0@ttn/devices/heltec-tcc/up")
 
+# Message callback
 def on_message(client, userdata, msg):
+    # Get message data
     payload = json.loads(msg.payload.decode('utf-8'))
     decoded_data_str = payload.get('uplink_message', {}).get('decoded_payload', {}).get('decodedData', "")
 
-    if decoded_data_str:
-        decoded_data = json.loads(decoded_data_str)
-        message_time = decoded_data.get('time', "")
-        aux = message_time
-        print(f"Message Time: {message_time}")
-    else:
-        print("Nada")
+    # Get timestamp
+    timestamp = datetime.now().strftime('%Y-%m-%d% H:%M')
+    
+    # Create the new entry
+    new_entry = {
+        'decodedData': decoded_data_str,
+        'timestamp': timestamp
+    }
+    
+    # Try open the file, if not exist save in a buffer
+    try:
+        with open(file_path, 'r') as file:
+            existing_data = json.load(file)
+    except FileNotFoudError:
+        existing_data=[]
+
+    # Append the new data
+    existing_data.append(new_entry)
+
+    # Write the data in the file
+    with open(file_path, 'w') as file:
+        json.dump(existing_data, file, indent=4)
+    
 
 client = mqtt.Client()
 
